fixed projection loading and added check for singular hessian in projection

lsdo_function_spaces/core/function.py
@@ -361,10 +361,13 @@ class Function:
                 )
 
             # Remove dof that are on constrant boundary and want to leave (active subspace method)
-            coorinates_to_remove_on_lower_boundary = np.logical_and(current_guess[points_left_to_converge] == 0, gradient > 0)
-            coorinates_to_remove_on_upper_boundary = np.logical_and(current_guess[points_left_to_converge] == 1, gradient < 0)
-            coorinates_to_remove_boolean = np.logical_or(coorinates_to_remove_on_lower_boundary, coorinates_to_remove_on_upper_boundary)
-            coordinates_to_keep_boolean = np.logical_not(coorinates_to_remove_boolean)
+            coordinates_to_remove_on_lower_boundary = np.logical_and(current_guess[points_left_to_converge] == 0, gradient > 0)
+            coordinates_to_remove_on_upper_boundary = np.logical_and(current_guess[points_left_to_converge] == 1, gradient < 0)
+            coordinates_to_from_zero_hessian_column = np.where(~hessian.any(axis=0))[0]
+            coordinates_to_remove_boolean = np.logical_or(coordinates_to_remove_on_lower_boundary, coordinates_to_remove_on_upper_boundary)
+            coordinates_to_remove_boolean[coordinates_to_from_zero_hessian_column] = True
+
+            coordinates_to_keep_boolean = np.logical_not(coordinates_to_remove_boolean)
             indices_to_keep = []
             for i in range(points_left_to_converge.shape[0]):
                 indices_to_keep.append(np.arange(self.space.num_parametric_dimensions)[coordinates_to_keep_boolean[i]])

lsdo_function_spaces/core/function_set.py
@@ -345,19 +345,20 @@ class FunctionSet:
 
     def _check_whether_to_load_projection(self, points:np.ndarray, direction:np.ndarray=None, grid_search_density_parameter:int=1,
                                          max_newton_iterations:int=100, newton_tolerance:float=1e-6) -> bool:
-        name_space = f'{self.name}_{self.coefficients.value}'
+        name_space = f'{self.name}'
 
         for function in self.functions:
             function_space = function.space
 
-            order = function_space.order
+            coefficients = function.coefficients.value
+            degree = function_space.degree
             coeff_shape = function_space.coefficients_shape
             knot_vectors_norm = round(np.linalg.norm(function_space.knots), 2)
 
-            # if f'{target}_{str(order)}_{str(coeff_shape)}_{str(knot_vectors_norm)}' in name_space:
+            # if f'{target}_{str(degree)}_{str(coeff_shape)}_{str(knot_vectors_norm)}' in name_space:
             #     pass
             # else:
-            name_space += f'_{str(order)}_{str(coeff_shape)}_{str(knot_vectors_norm)}'
+            name_space += f'_{str(coefficients)}_{str(degree)}_{str(coeff_shape)}_{str(knot_vectors_norm)}'
         
         long_name_space = name_space + f'_{str(points)}_{str(direction)}_{grid_search_density_parameter}_{max_newton_iterations}'
 