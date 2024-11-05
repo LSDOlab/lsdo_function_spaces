8e9a60b9ddaedcf6f146d16ae2c286419d4cf315
Michael Warner <hgxe14@gmail.com>
2024-11-05 10:50:05 -0800

    fixed import

--- lsdo_function_spaces/core/operations/operations.py
+++ lsdo_function_spaces/core/operations/operations.py
@@ -2,4 +2,4 @@ from .basic_ops import (add, sub, mult, div, negate, power, sqrt, exp, log,
                         absolute, maximum, minimum, average, sum, product,
                         tensordot, cross, norm,
                         sin, cos, tan, arcsin, arccos, arctan, sinh, cosh, tanh,
-                        bessel, concatenate)
+                        bessel)
