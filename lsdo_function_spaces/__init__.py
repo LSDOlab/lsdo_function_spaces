1a60393d3efacb555fc866f82ca23e8b7ca2c3fd
Andrew Fletcher <afletcher168@gmail.com>
2026-09-08 00:29:03 -0700

    make new import the default import

--- lsdo_function_spaces/__init__.py
+++ lsdo_function_spaces/__init__.py
@@ -21,8 +21,9 @@ from .core.spaces.tri_space import LinearTriangulationSpace
 
 # utilities
 from .utils.plotting_functions import plot_points, plot_curve, plot_surface, show_plot
-from .utils.file_io import import_file
+# from .utils.file_io import import_file
 from .utils.file_io_patched import import_file as import_file_patched
+from .utils.file_io_patched import import_file
 from .utils.utility_functions import create_b_spline_from_corners, create_enclosure_block
 
 # operations
